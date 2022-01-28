b4f7b5302b425fe1db889cec65266cd46e18a022
Markus Poutanen <25428918+MarkusPoutanen@users.noreply.github.com>
2022-01-28 21:54:13 +0200

    Add basic layout styles

--- resources/views/frontpage.php
+++ resources/views/frontpage.php
@@ -5,10 +5,14 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Simple PHP Framework</title>
+        <link rel="stylesheet" href="/assets/css/app.css?ver=0.1">
     </head>
+    
     <body>
-        <h1>Simple PHP Framework</h1>
-        <p>This is a Laravel-inspired simple custom MVC-based PHP framework.</p>
-        <p><?= date('Y') ?></p>
+        <main class="main">
+            <h1>Simple PHP Framework</h1>
+            <p>This is a Laravel-inspired simple custom MVC-based PHP framework.</p>
+            <p>Current date: <?= date('Y-m-d') ?></p>
+        </main>
     </body>
 </html>

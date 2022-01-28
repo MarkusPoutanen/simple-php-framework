76c875a8f1c30e8856e6e90000886493246eaad4
Markus Poutanen <25428918+MarkusPoutanen@users.noreply.github.com>
2022-01-28 22:26:44 +0200

    Remove phpinfo route and controller

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,9 @@
 
 use App\Route;
 
-use Controllers\InfoController;
 use Controllers\PageController;
 
 // Routes
-
-Route::get('info', [InfoController::class, 'info']);
 
 Route::get('/', [PageController::class, 'frontpage']);
 

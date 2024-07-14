61cce907039e4a2d56bca515768137a3316b1aa2
Markus Poutanen <25428918+MarkusPoutanen@users.noreply.github.com>
2024-07-14 12:04:35 +0300

    Helper: Add asset()
    This returns cache busting public path.

--- app/Helper.php
+++ app/Helper.php
@@ -18,13 +18,30 @@ class Helper
             include $full_path . '.php';
             return;
         }
-        
+
         if(file_exists($full_path . '.html'))
         {
             include $full_path . '.html';
             return;
         }
-        
+
         throw new \Exception('No view named "' . $name . '" in views folder');
+    }
+
+    /**
+     * Returns full public path for an asset path.
+     * @param string $asset_path Relative asset path in public directory.
+     * @return string Full public asset path.
+     */
+    public static function asset(string $asset_path): string
+    {
+        $file_path = __DIR__ . '/../public/' . $asset_path;
+
+        if(file_exists($file_path) === false)
+        {
+            return '';
+        }
+
+        return "/{$asset_path}?ver=" . md5_file($file_path);
     }
 }
